dc13afe414ede665ea8148ef5c903344c9926179
Arcangel Artigue <artigue_arcangel@hotmail.com>
2020-10-12 20:14:24 -0300

    feat: first commit

---
+++ src/main/java/com/example/gradlerestspringbootapi/GradleRestSpringbootApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradlerestspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleRestSpringbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleRestSpringbootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradlerestspringbootapi/GradleRestSpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlerestspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleRestSpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
